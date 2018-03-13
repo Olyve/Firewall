7a6e2b41ac1b466bf53f2479ef53757b6fbfd2a8
Sam Galizia <samgalizia@gmail.com>
2018-03-13 15:12:47 -0700

    Initial Commit

---
+++ Firewall/Scenes/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Firewall
+//
+//  Created by Sam Galizia on 3/8/18.
+//  Copyright © 2018 Sam Galizia. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+  
+  override func didMove(to view: SKView) {
+    
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+  }
+}
